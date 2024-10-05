98422bd7ba17e813f2249f5703d071df5e435c2d
aa <marakuyaspb@gmail.com>
2024-10-05 12:48:59 +0300

    replace popups component

--- cars/forms.py
+++ cars/forms.py
@@ -6,8 +6,9 @@ class CallMeForm(forms.ModelForm):
 	class Meta:
 		model = CallMe
 		fields = ['first_name', 'phone']
+		
 
 class CarSurvey_Full(forms.ModelForm):
 	class Meta:
-		model = CallMe
-		fields = ['first_name', 'phone']
+		model = CarSurvey_Full
+		fields = ['first_name', 'phone','car_characteristics' ,'country', 'when', 'payment_type', 'max_price','', 'trade_in','complectation','colors','need_casco','real_price']
