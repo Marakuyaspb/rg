f7ac309e36bffebd13dccbc9787f6498e7ef1435
aa <marakuyaspb@gmail.com>
2024-12-09 18:10:45 +0300

    add alzyload

---
+++ static/js/lazy_video_load.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const videos = document.querySelectorAll('video[data-src]');
+
+    const options = {
+        root: null, // Use the viewport as the container
+        threshold: 0.1 // Trigger when 10% of the video is visible
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                const video = entry.target;
+                const source = video.querySelector('source');
+                source.src = video.getAttribute('data-src');
+                video.load(); // Load the video
+                observer.unobserve(video); // Stop observing once loaded
+            }
+        });
+    }, options);
+
+    videos.forEach(video => {
+        observer.observe(video);
+    });
+});
